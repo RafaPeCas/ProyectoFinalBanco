cf9406a0515485982d905453fa7083930f6d73d4
Rafael Pedrosa <blas_tois@alumnos.ilernasevilla.com>
2023-12-13 15:26:04 +0100

    Estilos de admin aplicados

--- app/views/admin.php
+++ app/views/admin.php
@@ -19,6 +19,7 @@
 
     if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["estadoSolicitud"])) {
         $pedidita = $_POST["estadoSolicitud"];
+        echo "<p hidden id='estadoPedidita'>" . $pedidita . "";
     }
     $controlador = new SolicitudPrestamoController();
     $resultado = $controlador->mostrarSolicitudes($pedidita);
@@ -28,68 +29,81 @@
     echo "</header>";
     ?>
 
-    <main>
-        <form method="post" action="" id="formularioEstado">
-            <button type="submit" name="estadoSolicitud" value="aceptada">Aceptados</button>
-            <button type="submit" name="estadoSolicitud" value="pendiente">Pendientes</button>
-            <button type="submit" name="estadoSolicitud" value="rechazada">Rechazados</button>
-        </form>
-
-        <?php
-        if (isset($resultado) && is_array($resultado) && count($resultado) > 0) {
-            echo "        <table>
-                <tr>
-                    <th>ID Peticion</th>
-                    <th>ID Cuenta</th>
-                    <th>Cantidad</th>
-                    <th>Fecha Solicitud</th>
-                    <th>Estado</th>
-                    <th>Motivo</th>
-                    <th></th>
-                    <th></th>
-                </tr>";
-            foreach ($resultado as $fila) {
-                echo "<tr>";
-                echo "<td>" . $fila['id_peticion'] . "</td>";
-                echo "<td>" . $fila['id_cuenta'] . "</td>";
-                echo "<td>" . hexdec($fila['Cantidad']) . "</td>";
-                echo "<td>" . $fila['fecha_solicitud'] . "</td>";
-                echo "<td>" . $fila['estado'] . "</td>";
-                echo "<td>" . $fila['motivo'] . "</td>";
-
-                if ($fila['estado'] === "pendiente") {
-                    echo "<td><button onclick='aceptar(" . $fila['id_peticion'] . ", " . $fila['Cantidad'] . ")' name='aprobar'>Aprobar</button></td>";
-
-                    echo "<td><form method='post' action='../routes/procesarRechazarSolicitud.php'>";
-                    echo "<input type='hidden' name='id_peticion' value='" . $fila['id_peticion'] . "'>";
-                    echo "<button type='submit' name='rechazar'>Rechazar</button></form></td>";
-
-                    echo "</tr>";
-                }
-            }
-            echo "</table>";
-        } else {
-            if ($pedidita === "pendiente") echo "<tr><td colspan='8'>No hay prestamos pendientes</td></tr>";
-            if ($pedidita === "aceptada") echo "<tr><td colspan='8'>No hay prestamos aceptados</td></tr>";
-            if ($pedidita === "rechazada") echo "<tr><td colspan='8'>No hay prestamos rechazados</td></tr>";
-
-        }
-        ?>
-
-        <section class="ocultar" id="aceptarPrestamo">
-            <h2>Aceptar préstamo</h2>
-
-            <form action="../routes/procesarAceptarSolicitud.php" method="post">
-                <label for="mensualidad">Mensualidad:</label>
-                <input type="number" name="mensualidad" required>
-                <label for="tiempo">Tiempo (en dias):</label>
-                <input type="number" name="tiempo" required>
-                <input type='hidden' name='id_peticion' id='id_peticion' value=''>
-                <input type='hidden' name='cantidad' id='cantidad' value=''>
-                <button type="submit">Enviar Solicitud</button>
+    <main id="adminMain">
+        <section class="contenedorFormulario">
+            <h1>Solicitudes de prestamo</h1>
+            <form method="post" id="formularioEstado">
+                <button type="submit" name="estadoSolicitud" class="" value="aceptada" id="botonAceptados">Aceptados</button>
+                <button type="submit" name="estadoSolicitud" class="seleccionado" value="pendiente" id="botonPendientes">Pendientes</button>
+                <button type="submit" name="estadoSolicitud" class="" value="rechazada" id="botonRechazados">Rechazados</button>
             </form>
         </section>
-        <a href="welcome.php">welcome</a>
+        <section id="contenedorTabla">
+            <?php
+            if (isset($resultado) && is_array($resultado) && count($resultado) > 0) {
+                echo "<table id='tablaPeticiones'>
+                <thead>
+                    <tr>
+                        <th>ID Peticion</th>
+                        <th>ID Cuenta</th>
+                        <th>Cantidad</th>
+                        <th>Fecha Solicitud</th>
+                        <th>Estado</th>
+                        <th>Motivo</th>";
+                if ($pedidita === "pendiente") {
+                    echo "<th>Aprobar</th>
+                            <th>Rechazar</th>";
+                }
+                echo "</tr></thead><tbody>";
+
+                foreach ($resultado as $fila) {
+                    echo "<tr>";
+                    echo "<td>" . $fila['id_peticion'] . "</td>";
+                    echo "<td>" . $fila['id_cuenta'] . "</td>";
+                    echo "<td>" . hexdec($fila['Cantidad']) . "$</td>";
+                    echo "<td>" . $fila['fecha_solicitud'] . "</td>";
+                    echo "<td>" . $fila['estado'] . "</td>";
+                    echo "<td>" . $fila['motivo'] . "</td>";
+
+                    if ($fila['estado'] === "pendiente") {
+                        echo "<td><button onclick='aceptar(" . $fila['id_peticion'] . ", " . hexdec($fila['Cantidad']) . ")' name='aprobar'>Aprobar</button></td>";
+
+                        echo "<td><form method='post' action='../routes/procesarRechazarSolicitud.php'>";
+                        echo "<input type='hidden' name='id_peticion' value='" . $fila['id_peticion'] . "'>";
+                        echo "<button type='submit' name='rechazar'>Rechazar</button></form></td>";
+
+                        echo "</tr>";
+                    }
+                }
+
+                echo "</tbody></table>";
+            } else {
+                if ($pedidita === "pendiente") echo "<tr><td colspan='8'><h1 class='registroVacio'>No hay prestamos pendientes</h1></td></tr>";
+                if ($pedidita === "aceptada") echo "<tr><td colspan='8'><h1 class='registroVacio'>No hay prestamos aceptados</h1></td></tr>";
+                if ($pedidita === "rechazada") echo "<tr><td colspan='8'><h1 class='registroVacio'>No hay prestamos rechazados</h1></td></tr>";
+            }
+            ?>
+        </section>
+        <section class="ocultar" id="aceptarPrestamo">
+            <section id="formAceptarWrapper">
+                <h1>Aceptar préstamo</h1>
+
+                <form action="../routes/procesarAceptarSolicitud.php" method="post">
+                    <div class="form-item">
+                        <label for="mensualidad">Mensualidad:</label>
+                        <input type="number" name="mensualidad" required>
+                    </div>
+                    <div class="form-item">
+                        <label for="tiempo">Tiempo (en dias):</label>
+                        <input type="number" name="tiempo" required>
+                        <input type='hidden' name='id_peticion' id='id_peticion' value=''>
+                        <input type='hidden' name='cantidad' id='cantidad' value=''>
+                    </div>
+
+                    <button class="boton" type="submit">Aprobar préstamo</button>
+                </form>
+            </section>
+        </section>
     </main>
 </body>
 
--- public/js/admin.js
+++ public/js/admin.js
@@ -1,7 +1,32 @@
 "use strict"
 
-function aceptar(id_peticion, cantidad){
+function aceptar(id_peticion, cantidad) {
     aceptarPrestamo.classList.toggle("ocultar");
     document.getElementById('id_peticion').value = id_peticion;
     document.getElementById('cantidad').value = cantidad;
+}
+
+estilarBotonEstado();
+
+function estilarBotonEstado() {
+    let estadoPedidita = document.getElementById("estadoPedidita").innerHTML;
+
+    botonAceptados.classList.remove("seleccionado");
+    botonPendientes.classList.remove("seleccionado");
+    botonRechazados.classList.remove("seleccionado");
+
+    switch (estadoPedidita) {
+        case "aceptada":
+            botonAceptados.classList.add("seleccionado");
+            break;
+        case "rechazada":
+            botonRechazados.classList.add("seleccionado");
+            break;
+        case "pendiente":
+            botonPendientes.classList.add("seleccionado");
+            break;
+        default:
+            break;
+    }
+
 }
